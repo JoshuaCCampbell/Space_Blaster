bb6e4d1ed039832c6fbc8dcf305048a5c3b9a346
Joshua Campbell <joshuaccampbell@outlook.com>
2018-04-07 11:59:28 -0300

    Moved most of entity work to seperate functions

--- src/Main.c
+++ src/Main.c
@@ -26,12 +26,27 @@ void initialize_entities(
             int max_x,
             int max_y
         );
+void draw_entities(
+            Ship *p_player,
+            Blaster *p_playerblaster,
+            Ship *p_alien,
+            int *alive_aliens,
+            int max_x,
+            int max_y
+        );
+void move_entities(
+            Ship *p_player, 
+            Blaster *p_playerblaster, 
+            Ship *p_alien, 
+            int max_x, 
+            int max_y,
+            int ch, 
+            int tick
+        );
 
 int main(int argc, char **argv)
 {
     /* Counters */
-    int i;
-    int j;
     int tick = 0;
 
     /* Declare structs */
@@ -45,6 +60,7 @@ int main(int argc, char **argv)
     initialize_ncurses();
     getmaxyx(stdscr, max_y, max_x); 
 
+    /* Game setup */
     initialize_entities(&player, player_blaster, alien, max_x, max_y);
     
     /* Main game loop */
@@ -52,109 +68,25 @@ int main(int argc, char **argv)
     {
         int ch;
         int alive_aliens = 0;
-        clear();
         ch = getch();
+        clear(); /* clear the screen */
 
-        /* Draw player ship */
-        draw_ship(&player, PLAYER_SHAPE);
+        /* Draw entities */
+        draw_entities(&player, player_blaster, alien, &alive_aliens, max_x, max_y);
 
-        /* Draw alien ship */
-        for(i = 0; i < NUM_ALIENS; ++i)
+        /* Move entities */
+        move_entities(&player, player_blaster, alien, max_x, max_y, ch, tick);
+
+        /* Exit on F1 press */       
+        if(ch == KEY_F(1))
         {
-            if(alien[i].active)
-            {
-                draw_ship(&alien[i], ENEMY_SHAPE);
-                alive_aliens++;
-            }
-        }
-
-        /* Player blaster loop */
-        for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
-        {
-            /* Clear blaster once it's left the screen */
-            if(player_blaster[i].pos_y < 0)
-            {
-                player_blaster[i].active = 0;
-                player_blaster[i].pos_x = 0;
-                player_blaster[i].pos_y = 0;
-            }
-
-            /* Draw blaster */
-            if(player_blaster[i].active)
-            {
-                draw_blaster(&player_blaster[i]);
-                move_blaster(&player_blaster[i]);
-            }
-
-            /* Blaster hit detection */
-            for(j = 0; j < NUM_ALIENS; ++j)
-            {
-                if(
-                        player_blaster[i].active && 
-                        alien[j].active &&
-                        player_blaster[i].pos_x >= alien[j].pos_x &&
-                        player_blaster[i].pos_x <= (alien[j].pos_x + 2) &&
-                        player_blaster[i].pos_y <= alien[j].pos_y &&
-                        player_blaster[i].pos_y >= alien[j].pos_y - 2)
-                {
-                    alien[j].pos_x = 0;
-                    alien[j].pos_y = 0;
-                    alien[j].active = 0;
-                    player_blaster[i].active = 0;
-                }
-            }
-        } 
-
-        /* Movement keys and exit */
-        switch(ch)
-        {
-            case KEY_F(1):  /* Quit */
-                is_running = 0;
-            case KEY_LEFT:
-                move_ship(&player, max_x, 0);
-            case KEY_RIGHT:
-                move_ship(&player, max_x, 1); 
+            is_running = 0;
         }
         
-        /* Shoot blaster */
-        for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
-        {
-            if(ch == KEY_UP && player_blaster[i].active == 0)
-            {
-                player_blaster[i].active = 1;
-                player_blaster[i].pos_x = player.pos_x + 1;
-                player_blaster[i].pos_y = player.pos_y - 1;
-                break;
-            }
-        }
-
-        /* Move enemies here */
-        for(i = 0; i < NUM_ALIENS; ++i)
-        {
-            if(tick % 3 == 0)
-            {
-                alien[i].pos_y++;
-            }
-            
-            if(tick % 6 == 0 && alien[i].direction == 0)
-            {
-                alien[i].pos_x--;
-            }
-            if(tick % 6 == 0 && alien[i].direction == 1)
-            {
-                alien[i].pos_x++;
-            }
-
-            if(alien[i].pos_y >= max_y)
-            {
-                alien[i].pos_y = 0;
-                alien[i].pos_x = rand() % (max_x - 2);
-            }
-        }
-       
         /* Print number of active aliens */
         mvprintw(1, max_x - 10, "Aliens: %d", alive_aliens); 
 
+        /* Game loop timing */
         refresh();
         tick++;
         if(tick == TICK_BLOCK)
@@ -189,8 +121,10 @@ void initialize_entities(
     int alien_gap = (max_x - 10)  / NUM_ALIENS;
     int i;
 
+    /* Init player */
     init_ship(p_player, (max_x / 2) - 1, max_y - 1, 0, 1);
 
+    /* Init aliens */
     for(i = 0; i < NUM_ALIENS; ++i)
     {
         if(i == 0)
@@ -203,9 +137,137 @@ void initialize_entities(
         }
     }
 
+    /* Init blaster */
     for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
     {
         init_blaster(&p_playerblaster[i], 1);
     }   
 }
 
+void draw_entities(
+            Ship *p_player,
+            Blaster *p_playerblaster,
+            Ship *p_alien,
+            int *alive_aliens,
+            int max_x,
+            int max_y
+        )
+{
+    int i, j;
+
+    /* Draw player ship */
+    draw_ship(p_player, PLAYER_SHAPE);
+
+    /* Draw alien ship */
+    for(i = 0; i < NUM_ALIENS; ++i)
+    {
+        if(p_alien[i].active)
+        {
+            draw_ship(&p_alien[i], ENEMY_SHAPE);
+            ++*alive_aliens;
+        }
+    }
+
+    /* Player blaster loop */
+    for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
+    {
+        /* Clear blaster once it's left the screen */
+        if(p_playerblaster[i].pos_y < 0)
+        {
+            p_playerblaster[i].active = 0;
+            p_playerblaster[i].pos_x = 0;
+            p_playerblaster[i].pos_y = 0;
+        }
+
+        /* Draw blaster */
+        if(p_playerblaster[i].active)
+        {
+            draw_blaster(&p_playerblaster[i]);
+        }
+
+        /* Blaster hit detection */
+        for(j = 0; j < NUM_ALIENS; ++j)
+        {
+            if(
+                    p_playerblaster[i].active && 
+                    p_alien[j].active &&
+                    p_playerblaster[i].pos_x >= p_alien[j].pos_x &&
+                    p_playerblaster[i].pos_x <= (p_alien[j].pos_x + 2) &&
+                    p_playerblaster[i].pos_y <= p_alien[j].pos_y &&
+                    p_playerblaster[i].pos_y >= p_alien[j].pos_y - 2)
+            {
+                p_alien[j].pos_x = 0;
+                p_alien[j].pos_y = 0;
+                p_alien[j].active = 0;
+                p_playerblaster[i].active = 0;
+            }
+        }
+    }
+}
+
+void move_entities(
+            Ship *p_player, 
+            Blaster *p_playerblaster, 
+            Ship *p_alien, 
+            int max_x, 
+            int max_y, 
+            int ch,
+            int tick
+        )
+{
+    int i;
+
+    /* Move player */
+    switch(ch)
+    {
+        case KEY_LEFT:
+            move_ship(p_player, max_x, 0);
+        case KEY_RIGHT:
+            move_ship(p_player, max_x, 1); 
+    }
+
+    /* Move blaster */
+    for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
+    {
+        if(p_playerblaster[i].active)
+        {
+            move_blaster(&p_playerblaster[i]);
+        }
+    }
+        
+    /* Shoot blaster */
+    for(i = 0; i < NUM_PLAYER_SHOTS; ++i)
+    {
+        if(ch == KEY_UP && p_playerblaster[i].active == 0)
+        {
+            p_playerblaster[i].active = 1;
+            p_playerblaster[i].pos_x = p_player->pos_x + 1;
+            p_playerblaster[i].pos_y = p_player->pos_y - 1;
+            break;
+        }
+    }
+
+    /* Move aliens */
+    for(i = 0; i < NUM_ALIENS; ++i)
+    {
+        if(tick % 3 == 0)
+        {
+            p_alien[i].pos_y++;
+        }
+        
+        if(tick % 6 == 0 && p_alien[i].direction == 0)
+        {
+            p_alien[i].pos_x--;
+        }
+        if(tick % 6 == 0 && p_alien[i].direction == 1)
+        {
+            p_alien[i].pos_x++;
+        }
+
+        if(p_alien[i].pos_y >= max_y)
+        {
+            p_alien[i].pos_y = 0;
+            p_alien[i].pos_x = rand() % (max_x - 2);
+        }
+    }
+}
