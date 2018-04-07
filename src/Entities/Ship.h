0d5d573fd6ca959ec7ceef1b12f21f8988972336
Joshua Campbell <joshuaccampbell@outlook.com>
2018-04-07 10:09:42 -0300

    Added enemies

--- src/Entities/Ship.h
+++ src/Entities/Ship.h
@@ -2,8 +2,10 @@ typedef struct _ENTITY_Ship
 {
     int pos_x, pos_y;
     int vel_x, vel_y;
+    int direction;
+    int active;
 }Ship;
 
-void init_ship(Ship *ship, int start_x, int start_y, int velocity);
-void draw_ship(Ship *ship);
-void move_ship(Ship *ship, int max_x, char direction);
+void init_ship(Ship *ship, int start_x, int start_y, int direction, int velocity);
+void draw_ship(Ship *ship, const char *shape);
+void move_ship(Ship *ship, int max_x, int direction);
--- src/Globals.h
+++ src/Globals.h
@@ -9,5 +9,15 @@
 *                                               *
 *************************************************/
 
-#define SHIP_SHAPE "-^-"
-#define SHIP_WIDTH 3
+#define DELAY 35000
+#define TICK_BLOCK 60
+
+#define NUM_PLAYER_SHOTS 5
+#define NUM_ALIENS 5
+
+#define PLAYER_SHAPE "-^-"
+#define PLAYER_WIDTH 3
+
+#define ENEMY_SHAPE "=v="
+#define ENEMY_WIDTH "3"
+
